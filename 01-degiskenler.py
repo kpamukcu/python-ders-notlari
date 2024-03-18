60934539d501ce68c11fd86fabb6ef41c6ee2b99
kpamukcu <kapawebtasarim@gmail.com>
2024-03-19 00:21:31 +0300

    Düzeltme

--- 01-degiskenler.py
+++ 01-degiskenler.py
@@ -16,31 +16,6 @@ Değişken İsimleri Oluşturulurken Dikkat Edilmesi Gerekenler
 
     Bu listeyi bulabilmek için IDLE üzerinde import keyword yazdıktan sonra keyword.kwlist yazıp listelenebilir.
 
-4- Python dili kabul etse de Türkçe karkterlerin kullanımı tercih edilmemelidir.
-5- Büyük küçük harf duyarlılığı vardır yani A ile a farklı değişkenlerdir.
-
-
-'''
-
-
-'''
-
-DEĞİŞKENLER
-
-Metinsel ve/veya sayısal değerlerin atanması ile geçici olarak depolayan hafızalardır denilebilir. Atanan değerleri değiştirilebilir.
-
-Ör: a = 5   -> a ibaderi değişken olup = karakteri ise atama operatörüdür. 5 ise a değişkenine atanan değerdir.
-
-Değişken İsimleri Oluşturulurken Dikkat Edilmesi Gerekenler
-
-1- Bir değişkenin isminin ilk karakteri ya harf ya da alt çizgi(_) ile başlamalıdır.
-2- Bir değişken isminde alt çizgi haricinde boşluk veya diğer özel karakterler bulunmamalıdır.
-3- Bir değişken ismi python komutu olamaz.
-
-    ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
-
-    Bu listeyi bulabilmek için IDLE üzerinde import keyword yazdıktan sonra keyword.kwlist yazıp listelenebilir.
-
 4- Python dili kabul etse de Türkçe karakterlerin kullanımı tercih edilmemelidir.
 5- Büyük küçük harf duyarlılığı vardır yani A ile a farklı değişkenlerdir.
 
