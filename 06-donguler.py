8ba9c9a2a7471c97d32dde4bcea7e3554179082b
kpamukcu <kapawebtasarim@gmail.com>
2024-03-20 15:20:02 +0300

    Döngüler

---
+++ 06-donguler.py
@@ -0,0 +1,8 @@
+'''
+DÖNGÜLER(LOOP)
+
+- Tekrarlanması istenilen işlemler için kullanılırlar.
+- For ve While olmak üzere iki döngü tipi vardır.,
+- For döngüsünün döngü sayısı önceden bellidir.
+- While döngüsünün döngü ayısı önceden belli olmayıp koşula bağldır.
+'''
