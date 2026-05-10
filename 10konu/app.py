de782d5d3ebbca79398ff1e590b149bc3f3080f8
kpamukcu <kapawebtasarim@gmail.com>
2026-05-10 03:23:14 +0300

    streamlit

---
+++ 10konu/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Merhaba Streamlit ile Title Yazdım')
+st.write('Python ile web uygulamam')
+st.header('Bu bir başlık')
--- 10konu/main.py
+++ 10konu/main.py
@@ -1,3 +1,11 @@
 """ 
 Streamlit, Python ile çok hızlı şekilde web tabanlı arayüzler oluşturmanı sağlayan bir framework’tür.
+
+Özellikle:
+Veri analizi, Yapay zeka projeleri, Dashboard, Veri görselleştirme, Makine öğrenmesi demo uygulamaları için çok popülerdir.
+
+Kurulumu:
+pip install streamlit (Windows)
+pip3 install streamlit (Mac)
+
  """
