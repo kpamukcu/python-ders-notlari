c4e79f94a163586e4129c94352fdece9dbfefd3f
kpamukcu <kapawebtasarim@gmail.com>
2026-05-28 21:49:04 +0300

    Streamlit

--- 10konu/app.py
+++ 10konu/app.py
@@ -1,8 +1,79 @@
 import streamlit as st
 
+st.slider("Puan", 0, 100)
+
 st.title('Merhaba Streamlit ile Title Yazdım')  ### H1 etiketli başlık
 st.header('Bu bir başlık')                      ### H2 etiketli başlık
 st.subheader('Bu bir alt başlık')               ### H3 etiketli başlık
 
 st.write('Python ile web uygulamam')            ### p etiketli başlık 
 st.text('Text ile yazılan yazı')                ### etiketsiz yazı
+st.markdown('**Bu Kalın Yazıdır**')             ### Strong etiketi ile Kalın yazı yazmayı sağlar.
+
+isim = st.text_input('Adınızı Girin', help='Adınızı ve Soyadınızı Girin', placeholder='Adınız')           ### Kullanıcıdan bilgi almak için kullanlır.
+dogum = st.number_input('Doğum Yılınızı Girin', min_value=1900, max_value=2026)                           ### Kullanıcıdan sayısal bilgi almak için kullanlır.
+
+if isim and dogum:
+    if dogum<=2026:
+        yas = 2026 - dogum
+        if yas >= 18:
+            st.write(f'Sayın {isim}, {yas} yaşındasınız. Ehliyet Başvurusu Yapabilirsiniz.')
+        else:
+            st.write(f'Sayın {isim}, {yas} yaşındasınız. {18-yas} yıl sonra başvuru yapabilirsiniz.')
+    else:
+        st.write('Doğum Yılınızı Doğru Girin')
+else: ### Bu Blok Yazılmasa da Olur ###
+    st.write('')
+
+
+userName = st.text_input('Kullanıcı Adınızı Girin')
+password = st.text_input('Şifrenizi Girin', type='password')
+
+if userName == 'hayko' and password == '123':
+    adres = st.text_input('Teslimat Adresini Girin')
+    sehir = st.selectbox('Şehir', ['Ankara','Bursa','İstanbul','İzmir'], index=None, placeholder='Şehir Seçiniz')
+    cinsiyet = st.radio('Cinsiyet: ', ['Erkek','Kadın'])
+    mesaj = st.text_area('Not Ekleyin')
+    onay = st.checkbox('Kvkk Metnini Onaylıyorum')
+    
+
+    if st.button('Sipariş Ver'):
+        st.write(f'Sayın {userName} siparişiniz oanyalanmıştır.')
+        st.write('Bilgileriniz:')
+        st.write(f'Adres: {adres} / {sehir}')
+        st.write(f'Mesajınız: {mesaj}')
+        st.write(f'Cinsiyet: {cinsiyet}')
+        if onay:
+            st.write('Çerezlere Onay Verdiniz')
+        else:
+            st.write('Çerezlere Onay Vermediniz.')
+
+soru = st.sidebar.chat_input('Soru Sor')
+
+if soru:
+    st.sidebar.chat_message('user').write(soru)
+    st.sidebar.chat_message("assistant").write("Bunu aldım 👍")
+
+# st.sidebar.title('Menü')
+# st.sidebar.selectbox('Sayfalar',['Ana Sayfa','Blog','İletişim'])
+
+st.success('Kayıt Başarılı')
+
+kGiyim, eGiyim, cGiyim = st.tabs(['Kadın Giyim','Erkek Giyim','Çocuk Giyim'])
+
+with kGiyim:
+    st.write('En Kaliteli Kadın Giyim Ürünleri Bizde')
+
+eGiyim.write('En Kaliteli Erkek Giyim Ürünleri Bizde')
+
+cGiyim.write('En Kaliteli Çocuk Giyim ürünleri Bizde')
+
+kadi = ''
+sifre = ''
+
+signin,login = st.tabs(['Üye Ol','Giriş Yap'])
+
+with signin:
+    st.title('Üye Olun')
+    st.text_input('',placeholder='Adınız Soyadınız', label_visibility='collapsed')
+    st.text_input('',placeholder='E-Posta Adresiniz', label_visibility='collapsed')
--- 10konu/main.py
+++ 10konu/main.py
@@ -13,10 +13,73 @@ Streamlit ile oluşturulan bir web uygulamasını çalıştırabilmek için  ter
 
 Pip kurulumu yapıldıktan sonra proje içine Streamlit "import streamlit as st" şeklinde import edilir.
 
-st.title('Ana Başlık')      -> Büyük Başlık yazmayı sağlar.
-st.header('Başlık')         -> Başlık yazmayı sağlar.
-st.subheader('Alt Başlık')  -> Alt Başlık yazmayı sağlar.
+st.title('Ana Başlık')                  -> Büyük Başlık yazmayı sağlar.
+st.header('Başlık')                     -> Başlık yazmayı sağlar.
+st.subheader('Alt Başlık')              -> Alt Başlık yazmayı sağlar.
+st.write('Düz Yazı')                    -> p etiketli düz metin yazar 
+st.text('Düz Yazı')                     -> etiketsiz yazı
+st.markdown('**Kalın Düz Yazı**')       -> Strong etiketi ile Kalın yazı yazmayı sağlar.
 
+
+st.text_input('Label')                  -> Kullanıcıdan Bilgi Almak için kullanılır.
+
+| Parametre          | Açıklama                       |
+| ------------------ | ------------------------------ |
+| `label`            | Input başlığı                  |
+| `value`            | Varsayılan değer               |
+| `max_chars`        | Maksimum karakter              |
+| `placeholder`      | Gri açıklama yazısı            |
+| `type`             | `"default"` veya `"password"`  |
+| `help`             | Tooltip açıklaması             |
+| `disabled`         | Input’u pasif yapar            |
+| `label_visibility` | Label görünürlüğü              |
+| `key`              | Benzersiz anahtar              |
+| `autocomplete`     | Otomatik tamamlama             |
+| `on_change`        | Değişince fonksiyon çalıştırır |
+| `args`             | Fonksiyon argümanları          |
+
+
+
+st.number_input('Label')                -> Kullanıcıdan Sayısal Bilgi Almak içib Kullanılır.
+                                           min_value=1900, max_value=2026 parametreleri ile tanımlanabilir.
+
+
+| Parametre          | Açıklama           |
+| ------------------ | ------------------ |
+| `label`            | Input başlığı      |
+| `min_value`        | Minimum değer      |
+| `max_value`        | Maksimum değer     |
+| `value`            | Varsayılan değer   |
+| `step`             | Artış miktarı      |
+| `format`           | Görünüm formatı    |
+| `placeholder`      | Input içi açıklama |
+| `disabled`         | Pasif yapar        |
+| `label_visibility` | Label görünürlüğü  |
+| `key`              | Benzersiz anahtar  |
+| `help`             | Tooltip açıklaması |
+
+
+st.text_area('Label')                    -> Geniş Metin alanı için kullanılır.
+
+| Parametre          | Açıklama                        |
+| ------------------ | ------------------------------- |
+| `label`            | Alan başlığı                    |
+| `value`            | Varsayılan metin                |
+| `height`           | Alan yüksekliği (px)            |
+| `max_chars`        | Maksimum karakter               |
+| `placeholder`      | Gri açıklama yazısı             |
+| `help`             | Bilgi tooltip’i                 |
+| `disabled`         | Alanı pasif yapar               |
+| `label_visibility` | Label görünürlüğü               |
+| `key`              | Benzersiz ID                    |
+| `on_change`        | Değişince fonksiyon çalıştırır  |
+| `args`             | on_change fonksiyon argümanları |
+
+
+st.checkbox('Label')                                        -> Kullanıcıdan oany almak için kullanılır.
+st.selectbox('label',['Option 1','Option 2','Option 3'],index=None, placeholder='Seçiniz')    -> Kullanıcıdan seçim alınır
+st.button('Tıkla')      -> Form butonu oluşturur.
+st.radio('Label',['Option 1','Option 2'])   -> Kullanıcıdan tek bir seçim yapması istenir.
 
 
  """
